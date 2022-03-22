e0b88784bf76ec6dd2f45d9524d00f29a4b5964e
Sami Haroon <abdulsami.se@gmail.com>
2022-03-23 01:32:18 +0500

    minimal islands

--- graphs/7.MinimalIsland.js
+++ graphs/7.MinimalIsland.js
@@ -1,56 +1,45 @@
-const minimalIsland = (grid) =>{
-
-    // let islandSize = Number.MAX_SAFE_INTEGER;
-    // OR 
-    let islandSize = Infinity;
-    if(grid.length > 0 && grid[0].length > 0){
+const minimalIslandCount = (grid) =>{
     
-        const visited = new Set();
-        
-        for (let r = 0; r < grid.length; r++) {
-            for (let c = 0; c < grid[0].length; c++) {
-                let size = explore(grid, r, c, visited);
-                if(size < islandSize && size > 0)
-                {
-                    islandSize = size;
-                }
+    let minimalIsland = Infinity;
+
+    if(grid.length === 0 || grid[0].length === 0) return -1;
+
+    let visited = new Set();
+
+    for(let r  = 0; r < grid.length; r++){
+        for(let c = 0; c < grid[0].length; c++){
+            let isLandSize = exploreGrid(grid, r, c, visited);
+            if(isLandSize > 0 && isLandSize < minimalIsland){
+                minimalIsland = isLandSize;
             }
         }
-
-        return islandSize === Infinity ? -1: islandSize;
-
-    } else {
-        return -1;
     }
-    
+
+    return minimalIsland === Infinity ? 0: minimalIsland;
 };
 
-// r = 1
-// c = 24
-// pos = '1,24'
-
-// r = 12
-// c = 4
-// pos = '12,4'
-// gotcha = if you remove comma from key chances of collision are high i.e '1,24' and '12,4' will become same '124'
-const explore = (grid, r, c, visited) => {
-    //check inbounds
-    const rowInBounds = 0 <= r && r < grid.length;
-    const colInBounds = 0 <= c && c < grid.length;
+const exploreGrid = (grid, r, c, visited) => {
+    
+    let rowInBounds = (r >= 0 && r < grid.length);
+    let colInBounds = (c >= 0 && c < grid[0].length);
+    
+    // if not in boundaries of grid no searching of island
     if(!rowInBounds || !colInBounds) return 0;
 
+    // if Water do not explore
     if(grid[r][c] === 'W') return 0;
 
+    //if already explored no searching
+    if(visited.has(String(r + ',' + c))) return 0;
 
-    const pos = r + ',' + c;
-    if(visited.has(pos)) return 0;
-    visited.add(pos); // cycle prevention logic
+    // if not explored already, cycle prevention logic
+    visited.add(String(r + ',' + c)); 
 
     let size = 1;
-    size += explore(grid, r-1, c, visited); // going up
-    size += explore(grid, r+1, c, visited); // going down
-    size += explore(grid, r, c-1, visited); // going left
-    size += explore(grid, r, c+1, visited); // going right
+    size += exploreGrid(grid, r-1, c, visited); // top
+    size += exploreGrid(grid, r+1, c, visited); // bottom
+    size += exploreGrid(grid, r, c-1, visited); // left
+    size += exploreGrid(grid, r, c+1, visited); // right
 
     return size;
 };
@@ -64,6 +53,21 @@ const grid = [
     ['L','L','W','W','W'],
 ];
 
+
+console.log("Islands:", minimalIslandCount(grid)); // -> 3
+console.log("Islands:", minimalIslandCount([['W','W','W','W','W','W']])); // -> 0
+console.log("Islands:", minimalIslandCount([[]])); // -> -1
+
+// r = 1
+// c = 24
+// pos = '1,24'
+
+// r = 12
+// c = 4
+// pos = '12,4'
+// gotcha = if you remove comma from key chances of collision are high i.e '1,24' and '12,4' will become same '124'
+
+
 // common Gotcha for Set();
 // const s = new Set();
 // s.add([1,3]);
@@ -73,9 +77,6 @@ const grid = [
 // console.log(s.has('1,3'));
 
 // Island is vertically or horizontally connected region / segment
-console.log("Islands:", minimalIsland(grid)); // -> 3
-console.log("Islands:", minimalIsland([['W','W','W','W','W','W']])); // -> -1
-
 
 // complexity
 // r = # rows 
