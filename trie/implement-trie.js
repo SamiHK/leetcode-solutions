06779af3813e7a7703cc8231439c2abdfb203032
Sami Haroon <abdulsami.se@gmail.com>
2022-08-23 05:45:21 +0500

    Update implement-trie.js

--- trie/implement-trie.js
+++ trie/implement-trie.js
@@ -1,93 +1,118 @@
+ class TrieNode{
+    constructor(value){
+        this.value = value;
+        this.end = false;
+        this.branches = {};
+    }
+}
+class Trie{
+    
+    // R: O(1), S: O(1) 
+    constructor(){
+        this.root = new TrieNode("*");
+    }
+    
+    // R: O(W), S: O(W), here `W` is word
+    insert(word){
+        let root = this.root;
+        for(const c of word){
+            // if character does not exit we insert
+            if(!(c in root.branches)){
+                root.branches[c] = new TrieNode(c);
+            }
+            // moving to currently entered/next character
+            root = root.branches[c];
+        }
+        // if word ends that means its searchable
+        root.end = true;
+    }
+
+    // R: O(W), S: O(1), here `W` is word
+    search(word){
+        let root = this.root;
+        for(const c of word){
+            if(!(c in root.branches)){
+                // if character is not found in TrieNodes series
+                return false;
+            }
+            
+            // moving to currently entered/next character
+            root = root.branches[c];
+        }
+        // if search is complete check if same was inserted before
+        return root.end;
+    }
+
+    // R: O(W), S: O(1), here `W` is word
+    startsWith(prefix){
+        let root = this.root;
+        for(const c of prefix){
+            if(!(c in root.branches)){
+                // if character is not found in TrieNodes series
+                return false;
+            }
+            
+            // moving to currently entered/next character
+            root = root.branches[c];
+        }
+        
+        // branches always exist, no need to find the end
+        // ends are required for word completion 
+        return true;
+    }
+}
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
+
 // Prefix Tree
 // Autocomplete
 // Search
 // Dictionary
 
-class TrieNode{
-    constructor(value){
-        this.value = value;
-        this.children = {};
-        this.end = false;
-    }
-}
+// A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
 
-var Trie = function() {
-    this.root = new TrieNode("*")
-};
+// Implement the Trie class:
 
-
-/** 
- * @param {string} word
- * @return {void}
- */
- Trie.prototype.insert = function(word) {
-    
-    let curr = this.root;
-    // check each character of a word
-    for (const c of word){
-        // character can be inserted if not existing
-        if(!(c in curr.children)){
-            curr.children[c] = new TrieNode(c);
-        }
-        //moving current to next character
-        curr = curr.children[c];
-    }
-    curr.end = true;
-};
-
-/** 
- * @param {string} word
- * @return {boolean}
- */
- Trie.prototype.search = function(word) {
-    
-    let curr = this.root;
-    // move to check next character untill word end
-    for(const c of word){
-        // if character is not found return false
-        if(!(c in curr.children)){
-            return false;
-        }
-        //  move to next character
-        curr = curr.children[c];
-    }
-    return curr.end;
- };
-
-
+// Trie() Initializes the trie object.
+// void insert(String word) Inserts the string word into the trie.
+// boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+// boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
  
-/** 
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function(prefix) {
-    
-    let curr = this.root;
-    for(const c of prefix){
-        // if character is not found return false
-        if(!(c in curr.children)){
-            // we will not insert only check
-            return false;
-        }
-        // only check if children exist and not the word
-        curr = curr.children[c];
-    }
-    //children always exist, no need to find the end... ends are required for word completion
-    return true;
-};
 
+// Example 1:
 
-//  * Your Trie object will be instantiated and called as such:
-var obj = new Trie();
-let word = "sami";
-let prefix = "sam";
-obj.insert(word);
-console.log(obj);
-var param_2 = obj.search(word);
-console.log(param_2);
-var param_3 = obj.startsWith(prefix);
-console.log(param_3);
-param_3 = obj.startsWith("i");
-console.log(param_3);
-param_3 = obj.startsWith("sa");
-console.log(param_3);
+// Input
+// ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+// Output
+// [null, null, true, false, true, null, true]
+
+// Explanation
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // return True
+// trie.search("app");     // return False
+// trie.startsWith("app"); // return True
+// trie.insert("app");
+// trie.search("app");     // return True
+
+// //  * Your Trie object will be instantiated and called as such:
+// var obj = new Trie();
+// let word = "sami";
+// let prefix = "sam";
+// obj.insert(word);
+// console.log(obj);
+// var param_2 = obj.search(word);
+// console.log(param_2);
+// var param_3 = obj.startsWith(prefix);
+// console.log(param_3);
+// param_3 = obj.startsWith("i");
+// console.log(param_3);
+// param_3 = obj.startsWith("sa");
+// console.log(param_3);
